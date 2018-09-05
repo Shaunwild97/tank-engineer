b4e51a00b1109d34e04dc08bb6c747b410370a3e
Shaun Wild <shaun.wild@sainsburys.co.uk>
2018-09-05 13:49:30 +0100

    lombok

--- src/main/java/com/vobis/tankengineer/RayTrace.java
+++ src/main/java/com/vobis/tankengineer/RayTrace.java
@@ -1,63 +1,19 @@
 package com.vobis.tankengineer;
 
 import com.vobis.tankengineer.entity.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  *
  * @author Shaun
  */
+@Data
+@AllArgsConstructor
 public class RayTrace {
-
     private Vector2 origin;
     private Vector2 collision;
     private float direction;
     private float distance;
     private Entity entity;
-
-    public RayTrace(Vector2 origin, Vector2 collision, float direction, float distance, Entity entity) {
-        this.origin = origin;
-        this.collision = collision;
-        this.direction = direction;
-        this.distance = distance;
-        this.entity = entity;
-    }
-
-    public RayTrace() {
-    }
-
-    public Vector2 getOrigin() {
-        return origin;
-    }
-
-    public Vector2 getCollision() {
-        return collision;
-    }
-
-    public float getDirection() {
-        return direction;
-    }
-
-    public float getDistance() {
-        return distance;
-    }
-
-    public void setOrigin(Vector2 origin) {
-        this.origin = origin;
-    }
-
-    public void setCollision(Vector2 collision) {
-        this.collision = collision;
-    }
-
-    public void setDirection(float direction) {
-        this.direction = direction;
-    }
-
-    public void setDistance(float distance) {
-        this.distance = distance;
-    }
-
-    public void setEntity(Entity entity) {
-        this.entity = entity;
-    }
 }
